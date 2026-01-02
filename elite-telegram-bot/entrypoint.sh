c95f49a8ce2fbda58c843fa0b72ceb7b4e49d88a
Jacob Young <glock23makeyou@duck.com>
2026-01-02 12:51:04 -0600

    Update entrypoint.sh for improved error handling

--- elite-telegram-bot/entrypoint.sh
+++ elite-telegram-bot/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-set -x
-echo "PORT is $PORT"
+set -e
+
+echo "PORT is: ${PORT}"
+
 exec uvicorn src.app.web.api:app \
   --host 0.0.0.0 \
-  --port "$PORT" \
-  --log-level debug \
-  --lifespan off
+  --port ${PORT}
