f54df57bdaab79795cd32826bcf300097c283725
Jacob Young <glock23makeyou@duck.com>
2026-01-02 14:07:41 -0600

    Add Uvicorn server startup in main.py

--- elite-telegram-bot/src/app/bot/main.py
+++ elite-telegram-bot/src/app/bot/main.py
@@ -1,2 +1,8 @@
 from src.app.web.api import app
 
+import os
+import uvicorn
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
