d4d6983bd1426266f8687406e4eafe32057989a4
Jacob Young <glock23makeyou@duck.com>
2026-01-05 02:47:50 -0600

    Update entrypoint.sh

--- elite-telegram-bot/entrypoint.sh
+++ elite-telegram-bot/entrypoint.sh
@@ -3,4 +3,6 @@ set -e
 
 echo "Starting FastAPI on port 8080"
 
-exec CMD ["sh", "-c", "uvicorn src.app.bot.main:app --host 0.0.0.0 --port $PORT"]
+exec uvicorn src.app.bot.main:app 
+--host 0.0.0.0 \
+--port ${PORT}
