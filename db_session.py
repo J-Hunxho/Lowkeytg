8cf236e82e4cecd7399570f57886fff56e8285c4
Jacob Young <glock23makeyou@duck.com>
2026-03-30 15:06:56 -0500

    Add DbSessionMiddleware for database session management

---
+++ db_session.py
@@ -0,0 +1,11 @@
+from aiogram import BaseMiddleware
+from sqlalchemy.ext.asyncio import async_sessionmaker, AsyncSession
+
+class DbSessionMiddleware(BaseMiddleware):
+  def __init__(self, sessionmaker: async_sessionmaker[AsyncSession])
+    self.sessionmaker = sessionmaker
+
+  async def __call__(self, handler, event, data):
+    async with self.sessionmaker() as session:
+      data["session"] = session
+      return await handler(event, data)
